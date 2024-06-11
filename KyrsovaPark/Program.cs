5d140d7b1b9173fd24e04a9e909a49b8d03b2508
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-06-11 16:32:22 +0300

    Add and test UserInputResiver

---
+++ KyrsovaPark/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KyrsovaPark;
+
+Console.WriteLine("Hello, World!");
+
+var UserInputResiver = new UserInputResiver();
+
+Console.WriteLine("Program modes");
+Console.WriteLine("1 Input by yourself");
+Console.WriteLine("2 Input from generator");
+Console.WriteLine("3 Input from file");
+
+var workingMode = UserInputResiver.GetIntFromUser("Mode");
+
+switch (workingMode)
+{
+	case 1:
+        Console.WriteLine("Self");
+
+        break;
+    case 2:
+        Console.WriteLine("Generator");
+
+        break;
+    case 3:
+        Console.WriteLine("File");
+
+        break;
+    default:
+		break;
+}
---
+++ KyrsovaPark/UserInputResiver.cs
@@ -0,0 +1,17 @@
+﻿
+namespace KyrsovaPark
+{
+    public class UserInputResiver
+    {
+        public int GetIntFromUser(string variableName)
+        {
+            int variable = -1;
+            while (variable<=0)
+            {
+                Console.WriteLine($"Enter {variableName}:");
+                int.TryParse(Console.ReadLine(), out variable);
+            }
+            return variable;
+        }
+    }
+}
